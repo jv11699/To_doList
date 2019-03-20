2b0deb66b3ceef01d5fb1ce68066193985c6892a
jv11699 <janvolta@buffalo.edu>
2019-03-19 21:57:11 -0400

    initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add To-do...">
+            <input type="submit" value="Add To-do" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+    import uuid from 'uuid';
+    export default {
+        name: "AddTodo",
+        data(){
+            return{
+               title:'' //returns the data
+            }
+        },
+        methods: {
+            addTodo(e){
+                e.preventDefault();
+                const newTodo = {
+                    id: uuid.v4(),
+                    title: this.title,
+                    completed: false
+                }
+                //send up to parent
+                this.$emit('add-todo', newTodo);
+               // this.title = ''; //when clicked it will disappear
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    form{
+        display: flex;
+    }
+    input[type="text"]{
+        flex: 10;
+        padding: 5px;
+    }
+    input[type="submit"]{
+        flex:2;
+    }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!--
+         *This means it loops through the todos with the title
+         *ESlint error because when you loop through like this you need
+         *a unique key attached to this, much like in react-->
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo = "todo" v-on:del-todo="$emit('del-todo',todo.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+    export default {
+        name: "Todos",
+        components: {
+            TodoItem
+        },
+        props: ["todos"]
+    }
+</script>
+
+<style scoped>
+
+</style>
